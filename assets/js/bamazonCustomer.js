4b2c8666690f27ebebb0832cee4c19d5fb4360ff
Elias Roussos <eliasisaiah@outlook.com>
2019-09-17 15:11:32 -0400

    -rough initial table implemented

--- assets/js/bamazonCustomer.js
+++ assets/js/bamazonCustomer.js
@@ -1,11 +1,12 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Table = require("cli-table");
 
 //return results.map( r => r.item_name )
 /* const chosenItem = results.find( r => r.item_name === answer.choice); */
 
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
@@ -24,3 +25,94 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
     // connection.end();
 });
+
+
+const doQuery = function (command, input) {
+    console.log("adding item");
+
+    switch (command) {
+
+        case "artist":
+
+            let artistQuery = connection.query(
+                `SELECT * FROM top_songs_table WHERE artist_name='${input}'`,
+                (err, results) => {
+
+                    if (err) throw err;
+                    console.log(results);
+                }
+            );
+
+            console.log(artistQuery.sql);
+            break;
+
+        case "range":
+
+            let splitArr = input.split(",");
+            let rangeQuery = connection.query(
+
+                `SELECT * FROM top_songs_table WHERE ID BETWEEN ${splitArr[0]} AND ${splitArr[1]}`,
+                (err, results) => {
+
+                    if (err) throw err;
+                    console.log(results);
+                }
+            );
+
+            console.log(rangeQuery.sql);
+            break;
+
+        case "song":
+
+            let songQuery = connection.query(
+
+                `SELECT * FROM top_songs_table WHERE song_name='${input}'`,
+                (err, results) => {
+
+                    if (err) throw err;
+                    console.log(results);
+                }
+            );
+
+            console.log(songQuery.sql);
+            break;
+    }
+
+    // console.log(query.sql);
+}
+
+const productTable = new Table({
+    head: ['ID', 'NAME', 'PRICE($)'],
+    colWidths: [10, 200, 100]    
+});
+
+console.log(`
+██████╗  █████╗ ███╗   ███╗ █████╗ ███████╗ ██████╗ ███╗   ██╗
+██╔══██╗██╔══██╗████╗ ████║██╔══██╗╚══███╔╝██╔═══██╗████╗  ██║
+██████╔╝███████║██╔████╔██║███████║  ███╔╝ ██║   ██║██╔██╗ ██║
+██╔══██╗██╔══██║██║╚██╔╝██║██╔══██║ ███╔╝  ██║   ██║██║╚██╗██║
+██████╔╝██║  ██║██║ ╚═╝ ██║██║  ██║███████╗╚██████╔╝██║ ╚████║
+╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝                                                              
+            `)
+
+const initQuery = connection.query(
+
+    `SELECT * FROM products`,
+    (err, results) => {
+        if (err) throw err;
+        results.map((item, index) => {
+            
+            id = item.item_id;
+            name = item.product_name;
+            price = item.price;
+
+            productTable.push(
+                [id, name, price]
+            )
+        })
+        console.log(productTable.toString());
+        // console.log(results);
+    }
+);
+
+
